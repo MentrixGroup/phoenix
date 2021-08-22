9118cc0da016fd6e35cae3c7099b9e0c82def57e
Dominic Laycock <dtlaycock@users.noreply.github.com>
2021-08-22 15:34:07 -0400

    Add dynamodb resources to terraform

---
+++ iac/dynamo.tf
@@ -0,0 +1,24 @@
+module "dynamodb_tables" {
+  source = "terraform-aws-modules/dynamodb-table/aws"
+
+  for_each = var.dynamodb_tables
+
+  name     = each.key
+  hash_key = each.value.hash_key
+
+  attributes = [
+    {
+      name = each.value.attribute_name
+      type = each.value.attribute_type
+    }
+  ]
+
+  tags = local.common_tags
+
+}
+
+output "dynamo_table_arns" {
+  value = tomap({
+    for key, dynamodb_table_arn in module.dynamodb_tables : key => dynamodb_table_arn
+  })
+}
